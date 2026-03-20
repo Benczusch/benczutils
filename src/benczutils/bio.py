8e66facdd78a69aabacbacabf0fde4badda94dd3
Benczusch <benczusch@gmail.com>
2026-03-20 23:29:27 +0100

    [v0.1.3] Added codon list constant

--- src/benczutils/bio.py
+++ src/benczutils/bio.py
@@ -21,6 +21,26 @@ def readFasta(text):
     
     return genes
 
+# Stop Codon: X
+CODON_TABLE = {
+    "UUU": "F",      "CUU": "L",      "AUU": "I",      "GUU": "V",
+    "UUC": "F",      "CUC": "L",      "AUC": "I",      "GUC": "V",
+    "UUA": "L",      "CUA": "L",      "AUA": "I",      "GUA": "V",
+    "UUG": "L",      "CUG": "L",      "AUG": "M",      "GUG": "V",
+    "UCU": "S",      "CCU": "P",      "ACU": "T",      "GCU": "A",
+    "UCC": "S",      "CCC": "P",      "ACC": "T",      "GCC": "A",
+    "UCA": "S",      "CCA": "P",      "ACA": "T",      "GCA": "A",
+    "UCG": "S",      "CCG": "P",      "ACG": "T",      "GCG": "A",
+    "UAU": "Y",      "CAU": "H",      "AAU": "N",      "GAU": "D",
+    "UAC": "Y",      "CAC": "H",      "AAC": "N",      "GAC": "D",
+    "UAA": "X",      "CAA": "Q",      "AAA": "K",      "GAA": "E",
+    "UAG": "X",      "CAG": "Q",      "AAG": "K",      "GAG": "E",
+    "UGU": "C",      "CGU": "R",      "AGU": "S",      "GGU": "G",
+    "UGC": "C",      "CGC": "R",      "AGC": "S",      "GGC": "G",
+    "UGA": "X",      "CGA": "R",      "AGA": "R",      "GGA": "G",
+    "UGG": "W",      "CGG": "R",      "AGG": "R",      "GGG": "G", 
+}
+
 class MotifNotation:
     def __init__(self, notation_type, content):
         # notation type: 1 - positive ('A' or '[AB]')
